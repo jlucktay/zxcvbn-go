43edb403f5139caa1f1b21a7aedeb75bc55fdc96
James Lucktaylor <jlucktay+github@gmail.com>
2021-03-02 15:57:06 +0000

    fix: sort out lint issues

--- testapp/main.go
+++ testapp/main.go
@@ -11,16 +11,24 @@ import (
 func main() {
 	for {
 		fmt.Println("Enter password or Ctrl-c to exit:")
+
 		reader := bufio.NewReader(os.Stdin)
-		password, _ := reader.ReadString('\n')
+
+		password, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not read string: %v", err)
+
+			return
+		}
+
 		// password := "Testaaatyhg890l33t"
 
-		passwordStenght := zxcvbn.PasswordStrength(password, nil)
+		passwordStrength := zxcvbn.PasswordStrength(password, nil)
 
 		fmt.Printf("Password score    (0-4): %d\nEstimated entropy (bit): %f\nEstimated time to crack: %s\n\n",
-			passwordStenght.Score,
-			passwordStenght.Entropy,
-			passwordStenght.CrackTimeDisplay,
+			passwordStrength.Score,
+			passwordStrength.Entropy,
+			passwordStrength.CrackTimeDisplay,
 		)
 	}
 }
